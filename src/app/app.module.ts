e2bb5965e522982557cec54ae16430fcb2dc23a5
Michaela Davis and Zach Swanson <michaela@>
2017-04-05 11:24:42 -0700

    add routes to list pages, links in navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListPageComponent } from './list-page/list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,26 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component'
+import { ListPageComponent } from './list-page/list-page.component'
+
 const appRoutes: Routes = [
   {
-    path: '/',
-    component: 
-  }
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'project-list',
+    component: ListPageComponent
+  },
+  {
+    path: 'charitable-list',
+    component: ListPageComponent
+  },
+  {
+    path: 'idea-list',
+    component: ListPageComponent
+  },
 ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
